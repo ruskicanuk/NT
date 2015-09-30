c50feb96e6bd69277a791de8f47f0869bcd4a51d
dpatel <macpro@Macs-Mac-Pro.local>
2015-09-30 16:21:02 +0530

    How To Play integration

--- NapoleonTriumph/MenuView.swift
+++ NapoleonTriumph/MenuView.swift
@@ -10,14 +10,14 @@
 
 import SpriteKit
 import Foundation
-
+import UIKit
 class MenuView: SKScene {
     
     //This is a start game button.
     let startButton = SKSpriteNode(imageNamed:"start")
-
+    
     let leftButton = SKSpriteNode(imageNamed: "blue_marker")
-
+    
     let rightButton = SKSpriteNode(imageNamed: "blue_marker")
     
     let facebookButton = SKSpriteNode(imageNamed: "facebook")
@@ -25,8 +25,11 @@ class MenuView: SKScene {
     let twitterButton = SKSpriteNode(imageNamed: "twitter")
     
     let webButton = SKSpriteNode(imageNamed: "web")
-
+    
     var buttonSpacing: CGFloat = 100
+    
+    var viewController: GameViewController!
+    
     //buttonSpacing must be <150 so that it will show properly in iPhone 4s also
     
     override func didMoveToView(view: SKView) {
@@ -46,7 +49,7 @@ class MenuView: SKScene {
         //Left Side button
         leftButton.position = CGPointMake(CGRectGetMinX(self.frame) + buttonSpacing, CGRectGetMidY(self.frame))
         self.addChild(leftButton)
-
+        
         //Right Side button
         rightButton.position = CGPointMake(CGRectGetMaxX(self.frame) - buttonSpacing, CGRectGetMidY(self.frame))
         self.addChild(rightButton)
@@ -54,7 +57,7 @@ class MenuView: SKScene {
         //Facebook button
         facebookButton.position = CGPointMake(CGRectGetMinX(self.frame) + 40, CGRectGetMinY(self.frame) + 30)
         self.addChild(facebookButton)
-
+        
         //Twitter button
         twitterButton.position = CGPointMake(CGRectGetMinX(self.frame) + 80, CGRectGetMinY(self.frame) + 30)
         self.addChild(twitterButton)
@@ -62,7 +65,7 @@ class MenuView: SKScene {
         //Web button
         webButton.position = CGPointMake(CGRectGetMinX(self.frame) + 120, CGRectGetMinY(self.frame) + 30)
         self.addChild(webButton)
-
+        
     }
     
     
@@ -76,6 +79,7 @@ class MenuView: SKScene {
             else if leftButton.containsPoint(location)
             {
                 //Left button pressed
+                openHowToPlay()
             }
             else if facebookButton.containsPoint(location)
             {
@@ -95,11 +99,19 @@ class MenuView: SKScene {
         }
     }
     
+    private func openHowToPlay()
+    {
+        
+        let controller:HowToPlayViewController = viewController.storyboard?.instantiateViewControllerWithIdentifier("howtoplay") as! HowToPlayViewController
+        let navigationController:UINavigationController = UINavigationController.init(rootViewController: controller)
+        viewController.presentViewController(navigationController, animated: true, completion: nil)
+    }
+    
     private func startGame() {
         let gameScene = GameScene(size: view!.bounds.size)
         let transition = SKTransition.fadeWithDuration(0.15)
         view!.presentScene(gameScene, transition: transition)
         gameScene.scaleMode = .AspectFill
     }
-
+    
 }
